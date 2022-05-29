7c531e58e790a39263cf4a43faffc047e7d05be2
xiapeng <xiaxianpeng@autohome.com.cn>
2022-05-29 23:58:26 +0800

    源码分析

--- src/main/java/com/example/disruptor/heigh/multi/Producer.java
+++ src/main/java/com/example/disruptor/heigh/multi/Producer.java
@@ -10,11 +10,15 @@ public class Producer {
     }
 
     public void sendData(String uuid) {
+        // 1 生产着发送消息的时候，先从ringBuffer中获取一个可用的序号
         long sequence = ringBuffer.next();
         try {
+            // 2 根据这个序号找到对应的Evnet
             Order order = ringBuffer.get(sequence);
+            // 3 进行实际的赋值
             order.setId(uuid);
         } finally {
+            // 4 提交发布操作
             ringBuffer.publish(sequence);
         }
     }
